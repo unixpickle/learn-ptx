e4468e9d0d1f4c58b178465955b4bc0c0ce4f4d7
Alex Nichol <unixpickle@gmail.com>
2023-10-21 11:00:33 -0400

    per-warp reduction

--- learn_ptx/reduction.py
+++ learn_ptx/reduction.py
@@ -29,5 +29,29 @@ def reduction_bool_naive():
     print(f"true frac {results.astype(np.float32).mean()}")
 
 
+def reduction_bool_warp():
+    fn = compile_function("reduction_bool_warp.ptx", "reductionBoolWarp")
+    inputs = np.random.normal(size=[16384, 16384]).astype(np.float32)
+    threshold = np.median(inputs.min(axis=-1))
+    outputs = np.zeros([inputs.shape[0]], dtype=np.uint8)
+    input_buf = numpy_to_gpu(inputs)
+    output_buf = numpy_to_gpu(outputs)
+    with measure_time() as timer:
+        fn(
+            input_buf,
+            output_buf,
+            np.float32(threshold),
+            np.int64(inputs.shape[1]),
+            grid=(inputs.shape[0], 1, 1),
+            block=(32, 1, 1),
+        )
+    sync()
+    results = gpu_to_numpy(output_buf, outputs.shape, outputs.dtype)
+    expected = (inputs < threshold).any(axis=-1)
+    print(f"took {timer()} seconds")
+    print(f"disagreement frac {np.mean((expected != results).astype(np.float32))}")
+    print(f"true frac {results.astype(np.float32).mean()}")
+
+
 if __name__ == "__main__":
-    reduction_bool_naive()
+    reduction_bool_warp()
