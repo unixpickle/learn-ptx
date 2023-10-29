25fbb26a5da3ff6742704ef21ead812b293cac86
Alex Nichol <unixpickle@gmail.com>
2023-10-29 16:49:24 -0400

    reduction benchmark

--- learn_ptx/reduction.py
+++ learn_ptx/reduction.py
@@ -226,5 +226,58 @@ def reduction_all_max_multistep():
     assert results[0] == expected, f"{results[0]=} {expected=}"
 
 
+def reduction_all_max_flexible_multistep():
+    opt_reduce = compile_function(
+        "reduction_all_max_naive_opt_flexible.ptx", "reductionAllMaxNaiveOptFlexible"
+    )
+    small_reduce = compile_function(
+        "reduction_all_max_naive.ptx", "reductionAllMaxNaive"
+    )
+    inputs = np.random.uniform(size=[16384**2]).astype(np.float32)
+    inputs[16384 * 8192 + 1337] = 1.5  # hide a needle in the haystack
+    expected = np.max(inputs)
+    outputs = np.zeros([1024], dtype=np.float32)
+    input_buf = numpy_to_gpu(inputs)
+    warp_values = [1, 2, 4, 8, 16, 32]
+    block_values = [1, 2, 4, 8, 16, 32, 64]
+    output_grid = np.zeros([len(warp_values), len(block_values)])
+    for i, n_warps in enumerate(warp_values):
+        for j, n_blocks in enumerate(block_values):
+            output_buf = numpy_to_gpu(outputs)
+            with measure_time() as timer:
+                opt_reduce(
+                    input_buf,
+                    output_buf,
+                    np.int64((len(inputs) // n_blocks) // (n_warps * 32)),
+                    grid=(n_blocks, 1, 1),
+                    block=(32, n_warps, 1024 // (32 * n_warps)),
+                )
+                # Always reduce 1024 values, even though some of them
+                # may be zero.
+                small_reduce(
+                    output_buf,
+                    output_buf,
+                    np.int64(1),
+                    grid=(1, 1, 1),
+                    block=(1024, 1, 1),
+                )
+            sync()
+            results = gpu_to_numpy(output_buf, outputs.shape, outputs.dtype)
+            rate = ((int(np.prod(inputs.shape)) * 4) / timer()) / (2**30)
+            print(f"{n_warps=} {n_blocks=} GiB/s={rate}")
+            assert results[0] == expected, f"{results[0]=} {expected=}"
+            output_grid[i, j] = rate
+    rows = [["", *[f"{i} warps" for i in warp_values]]]
+    for label, row in zip(block_values, output_grid.T):
+        rows.append([f"{label} SMs", *[f"{x:.02f} GiB/s" for x in row]])
+    print("<table>")
+    for row in rows:
+        print("<tr>")
+        for item in row:
+            print(f"<td>{item}</td>")
+        print("</tr>")
+    print("</table>")
+
+
 if __name__ == "__main__":
-    reduction_all_max_naive_opt_novec()
+    reduction_all_max_flexible_multistep()
