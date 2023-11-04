f19892bfe20e879d8e1d600df20f4b55ab75be5c
Alex Nichol <unixpickle@gmail.com>
2023-11-04 14:42:38 -0400

    faster block sort

--- learn_ptx/sort.py
+++ learn_ptx/sort.py
@@ -77,5 +77,22 @@ def sort_bitonic_block():
     assert np.allclose(results, expected), f"\n{results=}\n{expected=}"
 
 
+def sort_bitonic_block_v2():
+    fn = compile_function("sort_bitonic_block_v2.ptx", "sortBitonicBlockV2")
+    inputs = np.random.normal(size=[16384, 1024]).astype(np.float32)
+    input_buf = numpy_to_gpu(inputs)
+    with measure_time() as timer:
+        fn(
+            input_buf,
+            grid=(inputs.shape[0], 1, 1),
+            block=(32, 32, 1),
+        )
+    sync()
+    results = gpu_to_numpy(input_buf, inputs.shape, inputs.dtype)
+    expected = np.sort(inputs, axis=-1)
+    print(f"took {timer()} seconds")
+    assert np.allclose(results, expected), f"\n{results=}\n{expected=}"
+
+
 if __name__ == "__main__":
-    sort_bitonic_warp_v3()
+    sort_bitonic_block_v2()
