f0d7d2d1be30887a739c8a2744b740afa53e01a6
Alex Nichol <unixpickle@gmail.com>
2023-11-12 13:06:36 -0500

    fps works

--- learn_ptx/fps.py
+++ learn_ptx/fps.py
@@ -7,7 +7,7 @@ from .context import compile_function, gpu_to_numpy, measure_time, numpy_to_gpu,
 
 def fps_block():
     fn = compile_function("fps_block.ptx", "farthestPointSampleBlock")
-    inputs = np.random.normal(size=[2**19, 3]).astype(np.float32)
+    inputs = np.random.normal(size=[2**16, 3]).astype(np.float32)
     outputs = np.zeros([4096, 3]).astype(np.float32)
     tmp = np.zeros([len(inputs)]).astype(np.float32)
     input_buf = numpy_to_gpu(inputs)
@@ -18,19 +18,21 @@ def fps_block():
             input_buf,
             tmp_buf,
             output_buf,
-            np.int32(len(inputs)),
-            np.int32(len(outputs)),
+            np.int64(len(inputs)),
+            np.int64(len(outputs)),
             grid=(1, 1, 1),
             block=(1024, 1, 1),
         )
     sync()
-    results = gpu_to_numpy(output_buf, inputs.shape, inputs.dtype)
+    results = gpu_to_numpy(output_buf, outputs.shape, outputs.dtype)
     print(f"took {timer():.05f} seconds on GPU")
     t1 = time.time()
-    expected = fps_on_cpu(input_buf, len(outputs))
+    expected = fps_on_cpu(inputs, len(outputs))
     t2 = time.time()
     print(f"took {(t2 - t1):.05f} seconds on CPU")
     print(f"maximum absolute error is {np.abs(results - expected).max()}")
+    print("results", results[:4])
+    print("expected", expected[:4])
 
 
 def fps_on_cpu(points: np.ndarray, n: int) -> np.ndarray:
@@ -39,7 +41,7 @@ def fps_on_cpu(points: np.ndarray, n: int) -> np.ndarray:
     dists = ((points - points[0]) ** 2).sum(-1)
     dists[0] = -1
     for i in range(1, n):
-        idx = np.argmin(dists)
+        idx = np.argmax(dists)
         point = points[idx]
         dists = np.minimum(dists, ((points - point) ** 2).sum(-1))
         dists[idx] = -1
