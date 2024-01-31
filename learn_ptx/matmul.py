c2aa6fa23ccb08f684c6fbcbc93b6971e7753847
Alex Nichol <unixpickle@gmail.com>
2024-01-30 19:19:33 -0500

    start working on tensorcore matmul

--- learn_ptx/matmul.py
+++ learn_ptx/matmul.py
@@ -130,6 +130,26 @@ def matmul_big_blocks_v5():
     generic_eval_matmul(call_fn)
 
 
+def matmul_wmma_v1():
+    fn = compile_function("matmul_wmma_v1.ptx", "wmmaMatmulV1")
+
+    def call_fn(A: np.ndarray, B: np.ndarray, A_buf: Any, B_buf: Any, out_buf: Any):
+        fn(
+            A_buf,
+            B_buf,
+            out_buf,
+            np.int32(A.shape[0] // 32),
+            grid=(
+                A.shape[0] // 32,
+                A.shape[1] // 32,
+                1,
+            ),
+            block=(32, 4, 1),
+        )
+
+    generic_eval_matmul(call_fn)
+
+
 def evaluate_matmul_fn(fn: Callable):
     def call_fn(A: np.ndarray, B: np.ndarray, A_buf: Any, B_buf: Any, out_buf: Any):
         block_size = 32
@@ -172,4 +192,4 @@ def generic_eval_matmul(fn: Callable, block_mult: int = 1):
 
 
 if __name__ == "__main__":
-    matmul_big_blocks_v5()
+    matmul_wmma_v1()
