e22a3c18df9e73193d96fc57f0115426f3f6f73d
Alex Nichol <unixpickle@gmail.com>
2024-02-01 12:48:36 -0500

    v2 kernel works

--- learn_ptx/matmul.py
+++ learn_ptx/matmul.py
@@ -150,6 +150,26 @@ def matmul_wmma_v1():
     generic_eval_matmul(call_fn)
 
 
+def matmul_wmma_v2():
+    fn = compile_function("matmul_wmma_v2.ptx", "wmmaMatmulV2")
+
+    def call_fn(A: np.ndarray, B: np.ndarray, A_buf: Any, B_buf: Any, out_buf: Any):
+        fn(
+            A_buf,
+            B_buf,
+            out_buf,
+            np.int32(A.shape[0] // 32),
+            grid=(
+                A.shape[0] // 32,
+                A.shape[1] // 32,
+                1,
+            ),
+            block=(32, 4, 1),
+        )
+
+    generic_eval_matmul(call_fn)
+
+
 def evaluate_matmul_fn(fn: Callable):
     def call_fn(A: np.ndarray, B: np.ndarray, A_buf: Any, B_buf: Any, out_buf: Any):
         block_size = 32
@@ -192,4 +212,4 @@ def generic_eval_matmul(fn: Callable, block_mult: int = 1):
 
 
 if __name__ == "__main__":
-    matmul_wmma_v1()
+    matmul_wmma_v2()
