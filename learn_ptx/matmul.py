131868a8f3d2104ea33bb6beecac54e1ae6a4d5b
Alex Nichol <unixpickle@gmail.com>
2024-02-15 22:59:12 -0500

    wmma v5

--- learn_ptx/matmul.py
+++ learn_ptx/matmul.py
@@ -210,6 +210,25 @@ def matmul_wmma_v4():
     generic_eval_matmul(call_fn)
 
 
+def matmul_wmma_v5():
+    fn = compile_function("matmul_wmma_v5.ptx", "wmmaMatmulV5")
+
+    def call_fn(A: np.ndarray, B: np.ndarray, A_buf: Any, B_buf: Any, out_buf: Any):
+        fn(
+            A_buf,
+            B_buf,
+            out_buf,
+            np.int32(A.shape[0] // 64),
+            grid=(
+                A.shape[0] // 64,
+                A.shape[1] // 64,
+                1,
+            ),
+            block=(32, 16, 1),
+        )
+
+    generic_eval_matmul(call_fn)
+
 def evaluate_matmul_fn(fn: Callable):
     def call_fn(A: np.ndarray, B: np.ndarray, A_buf: Any, B_buf: Any, out_buf: Any):
         block_size = 32
@@ -252,4 +271,4 @@ def generic_eval_matmul(fn: Callable, block_mult: int = 1):
 
 
 if __name__ == "__main__":
-    matmul_wmma_v4()
+    matmul_wmma_v5()
